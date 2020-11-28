f54fb6634092b4074d9fce4b9d23c97a1fe60212
renan028 <renan028@gmail.com>
2020-11-28 16:54:02 -0300

    add char to float conversions

--- include/remy_robot_control/utils.h
+++ include/remy_robot_control/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Geometry>
+#include <vector>
 
 namespace remy_robot_control {
 
@@ -11,7 +12,29 @@ namespace remy_robot_control {
   \param link_length length of the link
   \param offset offset between Xi-1 and Xi
 */
-Eigen::Affine3d DHToAffine(double theta, double alpha, double link_length, 
-    double offset = 0);
+Eigen::Affine3f DHToAffine(float theta, float alpha, float link_length, 
+    float offset = 0);
+
+/* Explicit pointer conversion char to float 
+ * \param buffer char[4]
+*/
+float charToFloat(unsigned char buffer[4]);
+
+/* Explicit pointer conversion float to char
+ * \param f float
+*/
+unsigned char* floatToChar(float f);
+
+/* It converts the vector<uchar> to Eigen::Vector3f
+ * \param v_uchar vector<uchar> dimension 12 (3*4)
+ * \return Eigen::Vector3f
+ */
+Eigen::Vector3f uchar3ToEigen3f(const std::vector<unsigned char>& v_uchar);
+
+/* It converts the Eigen::Vector3f to vector<uchar>
+ * \param vec3 Eigen::Vector3f 
+ * \return vector<uchar> dimension 12 (3*4)
+ */
+std::vector<unsigned char> eigen3fToUchar3(const Eigen::Vector3f& vec3);
 
 }
--- src/utils.cc
+++ src/utils.cc
@@ -2,22 +2,62 @@
 
 namespace remy_robot_control {
 
-Eigen::Affine3d DHToAffine(double theta, double alpha, double link_length, 
-    double offset) {
-  double ct = cos(theta);
-  double st = sin(theta);
-  double ca = cos(alpha);
-  double sa = sin(alpha);
+Eigen::Affine3f DHToAffine(float theta, float alpha, float link_length, 
+    float offset) {
+  float ct = cos(theta);
+  float st = sin(theta);
+  float ca = cos(alpha);
+  float sa = sin(alpha);
   
-  Eigen::Matrix3d R;
+  Eigen::Matrix3f R;
   R <<  ct, -st * ca,  st * sa, 
         st,  ct * ca, -ct * sa,
          0,   sa    ,  ca;
-  Eigen::Vector3d v = {link_length * ct, link_length * st, offset};
-  Eigen::Affine3d T = Eigen::Affine3d::Identity();
+  Eigen::Vector3f v = {link_length * ct, link_length * st, offset};
+  Eigen::Affine3f T = Eigen::Affine3f::Identity();
   T.linear() = R;
   T.translation() = v;
   return T;
 }
+
+
+float charToFloat(unsigned char c[4]) {
+  float f = *(float *)&c;
+  return f;
+}
+
+unsigned char* floatToChar(float f) {
+  unsigned char *s = (unsigned char *) &f;
+  return s;
+}
+
+Eigen::Vector3f uchar3ToEigen3f(const std::vector<unsigned char>& v_uchar) {
+  assert(v_uchar.size() == 12);
+  Eigen::Vector3f u = Eigen::Vector3f::Zero();
+  unsigned char c1[4] = {v_uchar[0], v_uchar[1], v_uchar[2], v_uchar[3]};
+  unsigned char c2[4] = {v_uchar[4], v_uchar[5], v_uchar[6], v_uchar[7]};
+  unsigned char c3[4] = {v_uchar[8], v_uchar[9], v_uchar[10], v_uchar[11]};
+  u[0] = charToFloat(c1);
+  u[1] = charToFloat(c2);
+  u[2] = charToFloat(c3);
+  return u;
+}
+
+std::vector<unsigned char> eigen3fToUchar3(const Eigen::Vector3f& vec3) {
+  std::vector<unsigned char> v_char;
+  v_char.reserve(12);
+  auto c1 = floatToChar(vec3[0]);
+  auto c2 = floatToChar(vec3[1]);
+  auto c3 = floatToChar(vec3[2]);
+  for (auto c : {c1, c2, c3}) {
+    v_char.push_back(c[0]);
+    v_char.push_back(c[1]);
+    v_char.push_back(c[2]);
+    v_char.push_back(c[3]);
+  }
+  return v_char;
+}
+
+
 
 }
