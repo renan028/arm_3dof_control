2ed4ae1e5e78543e3890bdb0ee00c81f44366e1a
renan028 <renan028@gmail.com>
2020-11-28 12:53:39 -0300

    add analytical control and fix damping singularity

--- include/remy_robot_control/control.h
+++ include/remy_robot_control/control.h
@@ -10,6 +10,8 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <functional>
+#include <chrono>
 
 // Eigen
 #include <Eigen/Geometry>
@@ -25,10 +27,15 @@ class Control {
   Connection connection;
   std::unique_ptr<Trajectory> trajectory;
   Robot model;
+  std::function<Eigen::Vector3d(Eigen::Vector3d, double)> control_;
 
   public:
     Control(const std::string& input);
     ~Control() = default;
+
+    /* This main method starts the controller. It periodically sends control 
+     * signals to the robot.
+    */
     void start();
 
     /* It gets the waypoints of the current trajectory
@@ -36,24 +43,53 @@ class Control {
     */
     std::vector<Eigen::Vector4d> getWaypoints();
 
+    /* Types of available Kinematics Control */
+    enum class ControlType {
+      feedfoward,
+      analytical
+    };
+
+    /* It sets the type of control. Default is Feedfoward */
+    void setControlStrategy(ControlType type);
+
+    /* It solves the Kinematic Control problem for the 3-DoF (elbow) remy robot.
+     * It computes the control signal for robot kinematics. There are some
+     * control strategies, thus please refer to the documentation of the one
+     * you want to use: 
+     * \sa feedforwardControl
+     * \sa pidControl
+     * \sa analyticalControl
+    */
+    Eigen::Vector3d computeVelocityControl(const Eigen::Vector3d& joints, 
+      double t);
+
   private:
     void readInput(const std::string& input);
   
   public:
     /*
-     * It solves the Kinematic Control problem for the 3-DoF (elbow) remy robot.
      * The robot motion can be described by: qdot = u, where u is the velocity 
-     * control signal (vetor) applied to the motor drive of each joint.
+     * control signal (vector) applied to the motor drive of each joint.
      * We have: xdot = J * qdot = J * u, where J is the Jacobian
      * Then: u = inv(J) * v(t), where v(t) is the cartesian control signal.
-     * e = xs(t) - x
+     * e = xs(t) - x, where xs is the desired cartesian position, x the current
      * edot = xsdot - v(t), then we may choose a proportional plus feedforward
      * control law:
      * v(t) = xsdot + Le, where L = lambda * I (identity matrix), lambda > 0
+     * Note that this is the Damping Method (DLS), thus if the robot starts at 
+     * a position where J(q0) is singular and v is in its nullspace, it may
+     * be impossible to find the inverse kinematics iteratively, and the control
+     * fails. The solution is to introduce a term belonging to the nullsapce of
+     * J:
+     * u = inv(J) * v + (I - inv(J) * J) * q0, where q0 is an arbitrary joint 
+     * velocities to avoid singularities.
      * \param joints the current robot joints
      * \param t current time (0 means start time)
     */
-    Eigen::Vector3d computeVelocityControl(const Eigen::Vector3d& joints, 
+    Eigen::Vector3d feedforwardControl(const Eigen::Vector3d& joints, 
+      double t);
+
+    Eigen::Vector3d analyticalControl(const Eigen::Vector3d& joints, 
       double t);
 
     
--- src/control.cc
+++ src/control.cc
@@ -10,6 +10,7 @@ namespace remy_robot_control {
 
 Control::Control(const std::string& input) {
   readInput(input);
+  setControlStrategy(ControlType::feedfoward);
 }
 
 void Control::readInput(const std::string& input) {
@@ -26,22 +27,68 @@ void Control::readInput(const std::string& input) {
   trajectory = std::make_unique<Trajectory>(std::move(waypoints));
 }
 
+void Control::setControlStrategy(ControlType type) {
+  switch (type)
+  {
+    case ControlType::feedfoward:
+      control_ = [this](const Eigen::Vector3d& joints, double t) {
+        return feedforwardControl(joints, t);
+      };
+      break;
+
+    case ControlType::analytical:
+      control_ = [this](const Eigen::Vector3d& joints, double t) {
+        return analyticalControl(joints, t);
+      };
+      break;
+    
+    default:
+      control_ = [this](const Eigen::Vector3d& joints, double t) {
+        return feedforwardControl(joints, t);
+      };
+      break;
+  }
+}
+
 std::vector<Eigen::Vector4d> Control::getWaypoints() {
   return trajectory->waypoints;
 }
 
-Eigen::Vector3d Control::computeVelocityControl(const Eigen::Vector3d& q,
+Eigen::Vector3d Control::computeVelocityControl(const Eigen::Vector3d& joints, 
     double t) {
+  return control_(joints, t);
+}
+
+Eigen::Vector3d Control::feedforwardControl(const Eigen::Vector3d& q,
+    double t) {
+  auto q0dot = Eigen::Vector3d(0.1, 0.1, 0.1);
   auto x = model.forwardKinematics(q);
-  trajectory->update(t);
-  auto v = trajectory->v + Eigen::Matrix3d::Identity() * (trajectory->x - x);
+  if (!trajectory->update(t)) {
+    return Eigen::Vector3d::Zero();
+  }
+  auto dx = trajectory->x - x;
+  auto v = trajectory->v + Eigen::Matrix3d::Identity() * dx;
   auto J = model.jacob(q);
   auto Jt = J.transpose();
   auto JJt = J * Jt;
   auto w = JJt.determinant();
-  auto alpha = (w >= 0.1) ? 0 : 10 * (1 - w/0.1) * (1 - w/0.1);
+  auto alpha = (w >= 0.001) ? 0 : 1 * (1 - w / 0.001) * (1 - w / 0.001);
   auto L = Eigen::Matrix3d::Identity() * alpha;
-  return Jt * (JJt + L).inverse() * v; 
+  auto Ji = Jt * (JJt + L).inverse();
+  return Ji * v + (Eigen::Matrix3d::Identity() - Ji * J) * q0dot; 
+}
+
+Eigen::Vector3d Control::analyticalControl(const Eigen::Vector3d& q,
+    double t) {
+  // TODO: change dt here
+  auto dt = 0.02;
+  if (!trajectory->update(t)) {
+    return Eigen::Vector3d::Zero();
+  }
+  auto xd = trajectory->x;
+  auto qd = model.inverseKinematics(xd[0], xd[1], xd[2]);
+  auto dq = qd - q;
+  return dq / dt; 
 }
 
 } // end namespace remy_robot_control
