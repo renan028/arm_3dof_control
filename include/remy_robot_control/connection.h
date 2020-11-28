ce0aa920692aada52d56bdd6c9f18001a30edab0
renan028 <renan028@gmail.com>
2020-11-28 19:44:42 -0300

    add connection

--- include/remy_robot_control/connection.h
+++ include/remy_robot_control/connection.h
@@ -1,17 +1,42 @@
+#pragma once
+
 #include <vector>
+#include <mutex>
+#include <atomic>
 
 class Connection
 {
-public:
-  Connection() {}
-  ~Connection() {}
+  std::vector<unsigned char> data_;
+  std::atomic<bool> opened_;
+  std::mutex mutex;
+  
+  public:
+    Connection() {
+      data_.reserve(12);
+    }
+    ~Connection() {}
 
-  int open(){};
-  int close(){};
+    int open(){
+      opened_ = true;
+    };
+    
+    int close(){
+      opened_ = false;
+    };
 
-  int send(std::vector<unsigned char> &data){};
-  //send data to the robot. use explicit pointer convertion
+    bool isOpened() {
+      return opened_;
+    }
 
-  int receive(std::vector<unsigned char> &data){};
-  //receive state of the robot. record to data. use explicit pointer convertion
+    int send(std::vector<unsigned char> &data){
+      std::lock_guard<std::mutex>lock(mutex);
+      data_ = data;
+    };
+    //send data to the robot. use explicit pointer convertion
+
+    int receive(std::vector<unsigned char> &data){
+      std::lock_guard<std::mutex>lock(mutex);
+      data = data_;
+    };
+    //receive state of the robot. record to data. use explicit pointer convertion
 };
